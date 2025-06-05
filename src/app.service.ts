0856add05e9b1fd65bdca7c05ef03a35de57f1e9
Francisco Higuera <franciscohiguera@gmail.com>
2025-06-05 15:40:11 -0500

    feat: deploy cicd

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,10 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getHealth(): { service: string; status: boolean } {
+    return {
+      service: 'nestjs-blog-backend-api',
+      status: true,
+    };
   }
 }
