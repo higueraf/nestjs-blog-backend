6b44063c828d244e2518114a784296da2309064a
Francisco Higuera <franciscohiguera@gmail.com>
2025-06-05 16:17:42 -0500

    feat: version in health

--- src/app.service.ts
+++ src/app.service.ts
@@ -5,7 +5,7 @@ export class AppService {
   getHealth(): { service: string; version: string, status: boolean } {
     return {
       service: 'nestjs-blog-backend-api',
-      version: '0.0.1',
+      version: '0.0.2',
       status: true,
     };
   }
