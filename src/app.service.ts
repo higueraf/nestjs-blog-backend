0ba13769999800990acb83963a3171a76eb5af63
Francisco Higuera <franciscohiguera@gmail.com>
2025-06-30 18:17:49 -0500

    fix: deploy

--- src/app.service.ts
+++ src/app.service.ts
@@ -5,7 +5,7 @@ export class AppService {
   getHealth(): { service: string; version: string, status: boolean } {
     return {
       service: 'nestjs-blog-backend-api',
-      version: '0.0.2',
+      version: '0.0.3',
       status: true,
     };
   }
