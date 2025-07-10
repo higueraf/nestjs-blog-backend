75e225a3ff36e2f179be0827128d381d483cf09e
Francisco Higuera <franciscohiguera@gmail.com>
2025-07-10 12:44:22 -0500

    testing cicd

--- src/app.service.ts
+++ src/app.service.ts
@@ -5,7 +5,7 @@ export class AppService {
   getHealth(): { service: string; version: string, status: boolean } {
     return {
       service: 'nestjs-blog-backend-api',
-      version: '2025.07.10',
+      version: '2025.07.10.12.44',
       status: true,
     };
   }
