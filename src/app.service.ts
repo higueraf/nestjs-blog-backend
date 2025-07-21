c8e08edd2b5397a5b27c35888d727f95dbbeda08
Francisco Higuera <franciscohiguera@gmail.com>
2025-07-21 15:51:39 -0500

    fix: cicd

--- src/app.service.ts
+++ src/app.service.ts
@@ -5,7 +5,7 @@ export class AppService {
   getHealth(): { service: string; version: string, status: boolean } {
     return {
       service: 'nestjs-blog-backend-api',
-      version: '2025.07.12.17.01',
+      version: '2025.07.21.15.20',
       status: true,
     };
   }
--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,10 @@ import { join } from 'path';
 import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule); // 👈 Correcto tipo de aplicación
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }));
   app.useGlobalFilters(new GlobalHttpExceptionFilter());
-  app.useStaticAssets(join(__dirname, '..', 'public')); // 👈 Habilita acceso público a /public
+  app.useStaticAssets(join(__dirname, '..', 'public'));
   app.enableCors();
   await app.listen(2999);
 }
